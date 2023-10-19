46390a0dddb3e369542ac06fa949eb84b32130b5
edupoly <info.edupoly@gmail.com>
2023-10-19 08:48:23 +0530

    sdcvxcv

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 function Todolist() {
-   
+    var ntRef = React.useRef();
     var [todos,setTodos] = React.useState(['get BMW','clean car']);
    
     function addTodo(){
-        var nt = document.getElementById("d1").value;
+        var nt = ntRef.current.value;
         setTodos([...todos,nt])
     }
     return (
         <div className='mybox'>
             <h1>Todolist</h1>
-            <input type="text" id='d1'/>
+            <input type="text" ref={ntRef}/>
             <button onClick={()=>{addTodo()}}>Add Todo</button>
             {
                 todos.map((t)=>{
